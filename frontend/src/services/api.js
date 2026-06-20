870e9b62c810743b078973a6d221a086e3cfe413
YaswanthChowdaryNadendla <nyaswanth5115@gmail.com>
2026-06-21 00:46:45 +0530

    Changed api url connected to frontend and backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://codeshell-w7n4.onrender.com';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
